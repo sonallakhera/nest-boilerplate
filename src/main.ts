fa10e3b8921f078d6719e8acd2041c80eb9cba65
Sonal Lakhera <sonal.lakhera@chingari.io>
2022-03-08 13:35:02 +0530

    update dotenv for local envs

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-
-import { AppModule } from './app.module';
-import { AppConfigService } from './configurations/app-config/app-config.service';
-
-import { name, version, description } from './../package.json';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
-  // App Config
-  const appConfig: AppConfigService = app.get('AppConfigService');
-
-  if (appConfig.swaggerAllowed) {
-    // Swagger
-    const config = new DocumentBuilder()
-      .setTitle(name)
-      .setDescription(description)
-      .setVersion(version)
-      .build();
-
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api-docs', app, document);
+const bootstrap = async () => {
+  if (process.env.APP_ENV !== 'local') {
+    // add ssm for non-local envs
+  } else {
+    const dotenv = await import('dotenv');
+    dotenv.config();
   }
+  await import('./index');
+};
 
-  await app.listen(3000);
-}
-
-bootstrap();
+bootstrap().catch((err) => {
+  console.error(err);
+});
